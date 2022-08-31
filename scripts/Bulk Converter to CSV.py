90ec1cc416f7fdcc50dbc88de8a0da460dea2e79
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-01 00:18:09 +0200

    modifiy bulk converter to take the path to xlxs files from gui

--- scripts/Bulk Converter to CSV.py
+++ scripts/Bulk Converter to CSV.py
@@ -9,17 +9,22 @@ def create_csv_folder(current_directory):
     return final_directory
 
 
-current_directory = os.getcwd()
-folders = []
-for folder in os.listdir(current_directory):
-    if '.' not in folder:
-        folders.append(folder)
 
-for folder in folders:
-    final_directory = create_csv_folder(current_directory + '/' + folder)
-    for xfile in os.listdir(current_directory + '/' + folder):
-        path = current_directory + '/' + folder + '/'
-        if xfile.endswith(".xlsx"):
-            df = pd.read_excel(path + xfile)
-            df.to_csv(final_directory + '/' + xfile[0:-5] + '.csv',index = None,header=True)
+
+def main(current_directory):
+    #current_directory = os.getcwd()
+    folders = []
+    for folder in os.listdir(current_directory):
+        if '.' not in folder:
+            folders.append(folder)
+
+    for folder in folders:
+        final_directory = create_csv_folder(current_directory + '/' + folder)
+        for xfile in os.listdir(current_directory + '/' + folder):
+            path = current_directory + '/' + folder + '/'
+            if xfile.endswith(".xlsx"):
+                df = pd.read_excel(path + xfile)
+                df.to_csv(final_directory + '/' + xfile[0:-5] + '.csv',index = None,header=True)
+
+
 
