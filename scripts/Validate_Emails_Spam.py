91e6edb528c09dd76614ecbbec6c8828feeb7705
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-06 19:40:03 +0200

    implement qthrid differently to solve the NoneType error for Validate_emails script

--- scripts/Validate_Emails_Spam.py
+++ scripts/Validate_Emails_Spam.py
@@ -5,7 +5,7 @@ import os
 
 
 
-def DirctoryPathToValidation(current_directory,plainTextEdit_Page6, QApplication):
+def DirctoryPathToValidation(current_directory):
     #current_directory = os.getcwd()
     final_directory = os.path.join(current_directory, r'valid emails')
     if not os.path.exists(final_directory):
@@ -16,16 +16,18 @@ def DirctoryPathToValidation(current_directory,plainTextEdit_Page6, QApplication
         if csvfile.endswith(".csv"):
             print('loading ' + csvfile + ' file: ' + str(j))
             s = 'loading ' + csvfile + ' file: ' + str(j)
-            plainTextEdit_Page6.appendPlainText(s)
-            QApplication.processEvents()
+            yield s
+            # plainTextEdit_Page6.appendPlainText(s)
+            # QApplication.processEvents()
 
             try:
                 df = pd.read_csv(current_directory + '/' + csvfile, encoding="ISO-8859-1", error_bad_lines=False,dtype=str)
             except:
                 print('problem reading ' + csvfile)
                 s = 'problem reading ' + csvfile
-                plainTextEdit_Page6.appendPlainText(s)
-                QApplication.processEvents()
+                yield s
+                # plainTextEdit_Page6.appendPlainText(s)
+                # QApplication.processEvents()
 
             if 'EMAIL' in df.columns:
                 email_col_name = 'EMAIL'
@@ -35,15 +37,17 @@ def DirctoryPathToValidation(current_directory,plainTextEdit_Page6, QApplication
 
             if ('EMAIL' in df.columns) or ('Email' in df.columns):
                 print('filtering emails.....')
-                plainTextEdit_Page6.appendPlainText('filtering emails.....')
-                QApplication.processEvents()
+                yield 'filtering emails.....'
+                # plainTextEdit_Page6.appendPlainText('filtering emails.....')
+                # QApplication.processEvents()
 
                 # delete invaled emails
                 df = df[df[email_col_name].notna()].reset_index(drop=True)
 
                 print('first stage....')
-                plainTextEdit_Page6.appendPlainText('first stage....')
-                QApplication.processEvents()
+                yield 'first stage....'
+                # plainTextEdit_Page6.appendPlainText('first stage....')
+                # QApplication.processEvents()
 
                 valid_email_list = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I',
                                     'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q',
@@ -62,12 +66,14 @@ def DirctoryPathToValidation(current_directory,plainTextEdit_Page6, QApplication
                     df = df.drop(df.index[invalid_index_list])
                 except:
                     print('first stage unsuccessful')
-                    plainTextEdit_Page6.appendPlainText('first stage unsuccessful')
-                    QApplication.processEvents()
+                    yield 'first stage unsuccessful'
+                    # plainTextEdit_Page6.appendPlainText('first stage unsuccessful')
+                    # QApplication.processEvents()
 
                 print('second stage....')
-                plainTextEdit_Page6.appendPlainText('second stage....')
-                QApplication.processEvents()
+                yield 'second stage....'
+                # plainTextEdit_Page6.appendPlainText('second stage....')
+                # QApplication.processEvents()
 
                 # drop empty emails
                 df = df[df[email_col_name].notna()].reset_index(drop=True)
@@ -76,12 +82,13 @@ def DirctoryPathToValidation(current_directory,plainTextEdit_Page6, QApplication
                     df = df[df[email_col_name].apply(lambda x: validate_email(x))]
                 except:
                     print('second stage unsuccessful')
-                    plainTextEdit_Page6.appendPlainText('second stage unsuccessful')
-                    QApplication.processEvents()
+                    # plainTextEdit_Page6.appendPlainText('second stage unsuccessful')
+                    # QApplication.processEvents()
 
                 print('third stage....')
-                plainTextEdit_Page6.appendPlainText('third stage....')
-                QApplication.processEvents()
+                yield 'third stage....'
+                # plainTextEdit_Page6.appendPlainText('third stage....')
+                # QApplication.processEvents()
 
                 try:
                     invalid_index_list = []
@@ -94,22 +101,26 @@ def DirctoryPathToValidation(current_directory,plainTextEdit_Page6, QApplication
                     df = df.drop(df.index[invalid_index_list])
                 except:
                     print('third stage unsuccessful')
-                    plainTextEdit_Page6.appendPlainText('third stage unsuccessful')
-                    QApplication.processEvents()
+                    yield 'third stage unsuccessful'
+                    # plainTextEdit_Page6.appendPlainText('third stage unsuccessful')
+                    # QApplication.processEvents()
 
                 df = df[df[email_col_name].notna()].reset_index(drop=True)
                 df.to_csv(final_directory + '/' + csvfile, index=None, header=True)
 
             else:
                 print('this file does not have EMAIL column')
-                plainTextEdit_Page6.appendPlainText('this file does not have EMAIL column')
-                QApplication.processEvents()
+                yield 'this file does not have EMAIL column'
+                # plainTextEdit_Page6.appendPlainText('this file does not have EMAIL column')
+                # QApplication.processEvents()
 
             print('===========================================================================================')
-            plainTextEdit_Page6.appendPlainText('===========================================================================================')
-            QApplication.processEvents()
+            yield '==========================================================================================='
+            # plainTextEdit_Page6.appendPlainText('===========================================================================================')
+            # QApplication.processEvents()
             j = j + 1
 
     print('program has finished execution')
-    plainTextEdit_Page6.appendPlainText('program has finished execution')
-    QApplication.processEvents()
+    yield 'program has finished execution'
+    # plainTextEdit_Page6.appendPlainText('program has finished execution')
+    # QApplication.processEvents()
