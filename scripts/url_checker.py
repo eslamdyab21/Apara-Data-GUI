1e40179ce52c678075a348462e812ae9c92451ba
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-17 14:24:06 +0200

    implement url_checker script

---
+++ scripts/url_checker.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+        
+
+def UrlToCheck(url):
+    proc = subprocess.Popen('dirhunt ' + url + ' --to-file report.json --delay 0.005', stdout=subprocess.PIPE, shell=True)
+    while True:
+        line = proc.stdout.readline()
+        if not line:
+            break
+        else: 
+            yield line
+            yield '------------------------------------------------------------------------------------------------'
+        
+
+val = UrlToCheck('https://jyplacements.com/')
+for value in val:
+    print(value)
